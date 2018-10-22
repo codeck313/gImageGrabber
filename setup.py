b32d0e669a9a13aa9f6c0a2c4c06356ea27952d6
codeeck <sakshamy2k@gmail.com>
2018-10-23 03:01:59 +0530

    version chnages

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 from setuptools import setup
 setup(
     name='gImageGrabber',
-    version='0.1.10',
+    version='0.1.10b',
     author='Saksham Sharma',
     author_email='codeck313@gmail.com',
     packages=['imggrabber', 'imggrabber.test'],
